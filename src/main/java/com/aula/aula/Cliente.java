2f5c88d86b73764b7348a78d6896eb6922791d5f
Cristiane Fernandes <cris.pessin@gmail.com>
2023-08-19 09:44:03 -0300

    Initial commit

---
+++ src/main/java/com/aula/aula/Cliente.java
@@ -0,0 +1,61 @@
+package com.aula.aula;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    private String nome;
+    private String sobrenome;
+
+    @Id
+    private Long id;
+
+    public Cliente(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
---
+++ src/main/java/com/aula/aula/ClienteControler.java
@@ -0,0 +1,44 @@
+package com.aula.aula;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/cliente")
+public class ClienteControler {
+    private ClienteRepository clienteRepository;
+
+    public ClienteControler(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @GetMapping
+    public Collection<Cliente> getNomeCliente(){
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    public Cliente criarCliente(@RequestBody Cliente cliente){
+        System.out.print(cliente);
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+
+    @PutMapping
+    public Cliente alterarCliente(@RequestBody Cliente cliente) {
+        System.out.println(cliente);
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+
+    @DeleteMapping
+    public void removeCliente(@RequestBody Cliente cliente) {
+        System.out.println(cliente);
+        clienteRepository.delete(cliente);
+    }
+}
